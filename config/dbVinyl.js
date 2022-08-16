561eaf824dff0ad925df2d815de6f4991ffdd62f
TOP-FLOOR-BOSS <engelliam71@gmail.com>
2022-08-16 09:09:27 +0200

    db

--- config/dbVinyl.js
+++ config/dbVinyl.js
@@ -1,28 +1,43 @@
-require('dotenv').config();
-const { createConnection } = require('mysql');
-// Create connection variable
-let connection;
-// Problem solved
-(function handleConnection() {
-    connection = createConnection({
-        host: process.env.host,
-        user: process.env.dbUser,
-        password: process.env.dbPassword,
-        port: process.env.dbPort,
-        database: process.env.dbName,
-        multipleStatements: true
-    });
-    
-    // connection.connect( (err)=> {
-    //     if(err) throw err 
-    // });
-    
-    // connection.on('error', (err)=> {
-    //     if(err.code === 'PROTOCOL_CONNECTION_LOST'){
-    //         handleConnection();
-    //     }else {
-    //         throw err;
-    //     }
-    // })    
-})();
-module.exports = connection;
+// const { createConnection } = require("mysql");
+// // Create connection variable
+// let connection;
+// // Problem solved
+// (function handleConnection() {
+//   connection = createConnection({
+//     host: process.env.host,
+//     user: process.env.dbUser,
+//     password: process.env.dbPassword,
+//     port: process.env.dbPort,
+//     database: process.env.dbName,
+//     multipleStatements: true,
+//   });
+
+//   // connection.connect( (err)=> {
+//   //     if(err) throw err
+//   // });
+
+//   // connection.on('error', (err)=> {
+//   //     if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+//   //         handleConnection();
+//   //     }else {
+//   //         throw err;
+//   //     }
+//   // })
+// })();
+// module.exports = connection;
+
+const mysql = require("mysql");
+require("dotenv").config();
+const { createPool } = require("mysql");
+// Here to is use across multiple files. Used to make SQL queries to DB
+const con = createPool({
+  host: process.env.host,
+  user: process.env.dbUser,
+  password: process.env.dbPassword,
+  port: process.env.dbPort,
+  database: process.env.dbName,
+  multipleStatements: true,
+  connectionLimit: 10,
+});
+
+module.exports = con;
